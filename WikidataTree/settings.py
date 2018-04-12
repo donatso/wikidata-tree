5334c9c4df56bd0d611dee5018323e42d728b55c
donatso <soricdonat@gmail.com>
2018-04-12 10:33:03 +0200

    static_root

--- WikidataTree/settings.py
+++ WikidataTree/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
